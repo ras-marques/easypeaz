c439986c962fed21cef9f71d61e183509f750790
Ruben Marques <rubenmarques91@gmail.com>
2018-05-07 18:15:25 +0000

    First site draft with starting ingredient list

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-var express = require('express')
-var app = express()
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+const bodyParser = require("body-parser");
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.listen(3000)
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// add recipes page
+app.get('/add_recipes', function(req, res) {
+    res.render('pages/add_recipes');
+});
+
+app.post('/add_recipes', function(req, res) {
+    var recipe = req.body.recipe;
+    var tuna = req.body.tuna;
+    console.log(recipe)
+});
+
+app.listen(3000);
+console.log('listening on port 3000');
