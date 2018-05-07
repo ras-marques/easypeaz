30e638bb0a58b01bcd59368e27aebdb0a32eb7ef
Ruben Marques <rubenmarques91@gmail.com>
2018-05-07 08:12:36 +0000

    Removes heroku install file after install

--- deployment/startup_configuration.sh
+++ deployment/startup_configuration.sh
@@ -20,6 +20,7 @@ echo "export EDITOR=vim" >> /etc/profile
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-arm.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xvzf heroku-linux-arm.tar.gz -C /usr/local/lib
+rm heroku-linux-arm.tar.gz
 ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 reboot
